ff8e5f810ad88da5fd81c9262fbe2e3b7cd3f7de
noname dude <73312551+notsleepingcoder@users.noreply.github.com>
2024-08-14 19:45:19 +0300

    Add files via upload

---
+++ copy_groups.ps1
@@ -0,0 +1,22 @@
+﻿# Пользователь с которого берем группы
+$sourceUser = "USER1"
+
+# Пользователь которому эти группы даем
+$destinationUser = "USER2"
+
+# Прописываем контроллер домена
+$domainController = "DOMAIN.CONTROLLER"
+
+# Получение всех групп, к которым принадлежит исходный пользователь
+$groups = Get-ADPrincipalGroupMembership -Server "$domainController" -Identity $sourceUser
+
+foreach ($group in $groups) {
+    # Добавление целевого пользователя ко всем группам исходного пользователя
+    Try {
+        Add-ADGroupMember -Identity $group -Members $destinationUser -ErrorAction Stop
+        Write-Host "Пользователь $destinationUser добавлен в группу $($group.Name)."
+        # Игнорирование ошибок, если пользователь уже состоит в группе
+    } Catch {
+        Write-Host "Ошибка: Не удалось добавить пользователя $destinationUser в группу $($group.Name) - $_"
+    }
+}
