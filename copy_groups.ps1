ba55c496e8849195ab93358fb7e9d620922c585c
noname dude <73312551+notsleepingcoder@users.noreply.github.com>
2024-08-15 12:28:50 +0300

    Update copy_groups.ps1

--- copy_groups.ps1
+++ copy_groups.ps1
@@ -1,21 +1,22 @@
-﻿# Пользователь с которого берем группы
+﻿# Change USER1 in $sourceUser to source user's sAMAccountname
 $sourceUser = "USER1"
 
-# Пользователь которому эти группы даем
+# Change USER2 in $destinationUser to destination user's sAMAccountname
 $destinationUser = "USER2"
 
-# Прописываем контроллер домена (если необходимо)
+# Change DOMAIN.CONTROLLER in $domainController to your full domain address 
+# (not neccessary, use it if you have more than one domain controller)
 $domainController = "DOMAIN.CONTROLLER"
 
-# Получение всех групп, к которым принадлежит исходный пользователь
+# Receiving all of source user's policy groups
 $groups = Get-ADPrincipalGroupMembership -Server "$domainController" -Identity $sourceUser
 
 foreach ($group in $groups) {
-    # Добавление целевого пользователя ко всем группам исходного пользователя
+    # Adding source user's to destination user
     Try {
         Add-ADGroupMember -Identity $group -Members $destinationUser -ErrorAction Stop
         Write-Host "User $destinationUser has been added in $($group.Name)."
-        # Игнорирование ошибок, если пользователь уже состоит в группе
+        # Ignoring errors, if user already have policy groups from the list
     } Catch {
         Write-Host "Error: Can not add user $destinationUser in a group $($group.Name) - $_"
     }
